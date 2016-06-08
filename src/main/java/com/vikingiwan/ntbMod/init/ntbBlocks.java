93dd212b03728c8666e670dd82c7e5082563cc5f
vikingiwan <vikingiwan@gmail.com>
2016-06-08 03:05:39 -0400

    .

--- src/main/java/com/vikingiwan/ntbMod/init/ntbBlocks.java
+++ src/main/java/com/vikingiwan/ntbMod/init/ntbBlocks.java
@@ -1,5 +1,30 @@
 package com.vikingiwan.ntbMod.init;
 
+import com.vikingiwan.ntbMod.client.Reference;
+
+import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+
 public class ntbBlocks {
 
+	
+	
+	public static void init() {
+		
+	}
+	
+	public static void register() {
+		
+	}
+	
+	public static void registerRenders() {
+		
+	}
+	
+	public static void registerRender(Block block) {
+		Item item = Item.getItemFromBlock(block);
+		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, 0, new ModelResourceLocation(Reference.MOD_ID + ":" + item.getUnlocalizedName().substring(5), "inventory"));
+	}
 }
--- src/main/java/com/vikingiwan/ntbMod/proxy/ClientProxy.java
+++ src/main/java/com/vikingiwan/ntbMod/proxy/ClientProxy.java
@@ -1,10 +1,12 @@
 package com.vikingiwan.ntbMod.proxy;
 
+import com.vikingiwan.ntbMod.init.ntbBlocks;
 import com.vikingiwan.ntbMod.init.ntbItems;
 
 public class ClientProxy extends CommonProxy {
 	@Override
 	public void registerRenders() {
 		ntbItems.registerRenders();
+		ntbBlocks.registerRenders();
 	}
 }
